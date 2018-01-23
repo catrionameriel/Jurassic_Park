c9c634f9f7ccbdfea45b36ca1d9c1d1f5a8df9e3
catrionameriel <catriona.meriel@gmail.com>
2018-01-23 16:51:22 +0000

    added enclosure empty test

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -1,3 +1,19 @@
 const assert = require('assert');
 const Dinosaur = require('../dinosaur');
 const Park = require('../park');
+
+describe('Park', function(){
+
+  let park;
+  let dinosaur;
+
+  beforeEach(function() {
+    park = new Park();
+    dinosaur = new Dinosaur('Tyrannosaurus', 2);
+  })
+
+  it('park starts with no dinosaurs', function() {
+    assert.strictEqual(park.enclosure.length, 0)
+  })
+
+})
