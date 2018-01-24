44c0fe2b76a182e03057c24ef3f46992a54b5659
catrionameriel <catriona.meriel@gmail.com>
2018-01-24 10:25:16 +0000

    refactored method in park

--- park.js
+++ park.js
@@ -6,15 +6,25 @@ Park.prototype.addDinosaur = function (dinosaur) {
   this.enclosure.push(dinosaur);
 };
 
+// Park.prototype.removeDinosaurByType = function (type) {
+//   for (let dinosaur of this.enclosure) {
+//     if (type === dinosaur.type) {
+//       let index = this.enclosure.indexOf(dinosaur);
+//       this.enclosure.splice(index, 1);
+//     }
+//   }
+// };
+//don't remove while looping! push the one's you are keeping into a new array and then set the enclosure to that array.
+
 Park.prototype.removeDinosaurByType = function (type) {
+  let dinosaursOfRightType = []
   for (let dinosaur of this.enclosure) {
     if (type === dinosaur.type) {
-      let index = this.enclosure.indexOf(dinosaur);
-      this.enclosure.splice(index, 1);
+      dinosaursOfRightType.push(dinosaur)
     }
+    this.enclosure = dinosaursOfRightType;
   }
 };
-//don't remove while looping! push the one's you are keeping into a new array and then set the enclosure to that array.
 
 Park.prototype.getDinosaursByNumberOfChildren = function (numberToCheck) {
   let dinosaursToReturn = [];
